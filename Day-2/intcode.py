c4f3cfa912671f8f7e754cbcd5bde06943a94a4d
theNerdyFeline <jmw5050@gmail.com>
2019-12-04 16:54:31 -0800

    Day 2 part2 done

--- Day-2/intcode.py
+++ Day-2/intcode.py
@@ -1,7 +1,8 @@
 from numpy import loadtxt
+from copy import copy
+
 data = loadtxt('data.txt', comments='#', dtype='int', delimiter=',', unpack=False)
-#data = [1, 1, 1, 4, 99, 5, 6, 0, 99]
-#print(data)
+address = 19690720
 
 def add(x, y):
 	return x+y
@@ -9,42 +10,41 @@ def add(x, y):
 def mult(x, y):
 	return (x * y)
 
-def rep(c, t):
-	try:
-		data[c] = t
-	except  IndexError:
-		pass
-
 def find_vals():
-	for n in range(0, 9):
-		for v in range(0, 9): 
-			grav_asst(n, v)
-			if data[0] == 19690720: 
-				print(data[0], data[1], data[2])
+	for n in range(0, 99):
+		for v in range(0, 99): 
+			d = copy(data)
+			grav_asst(n, v, d)
+			if d[0] == address: 
+				print(d[0], d[1], d[2])
 			else:
-				print(n, v, data[0])
-				break
+				#print('vals', d[0])
+				pass	
 
-def grav_asst(n, v):
-	data[1] = n
-	data[2] = v
+def grav_asst(n, v, rd):
+	def rep_in(curr, t):
+		try:
+			rd[curr] = t
+		except IndexError:
+			pass
+	rd[1] = n
+	rd[2] = v
 	i = 0
 	while i < len(data):
-		if data[i] == 99: break
+		if rd[i] == 99: break
 		else:
-			x = data[i+1]
-			y = data[i+2]
-			if data[i] == 1:
-				s = add(data[x], data[y])
-				c = data[i+3]
-				rep(c, s)
-			elif data[i] == 2:
-				p = mult(data[x], data[y])	
-				curr = data[i+3]
-				rep(curr, p)
+			x = rd[i+1]
+			y = rd[i+2]
+			if rd[i] == 1:
+				s = add(rd[x], rd[y])
+				c = rd[i+3]
+				rep_in(c, s)
+			elif rd[i] == 2:
+				p = mult(rd[x], rd[y])	
+				curr = rd[i+3]
+				rep_in(curr, p)
 		i += 4
-	print(data[0])
-	return data[0]
+	return rd[0]
 
 find_vals()
-#grav_asst(52, 6)
+#grav_asst(4, 6)
