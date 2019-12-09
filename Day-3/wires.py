4a1b3c85b283b846c15dacc7e87f95966c73107b
theNerdyFeline <jmw5050@gmail.com>
2019-12-09 11:52:12 -0800

    Day 3 Part 1 get grid points works

--- Day-3/wires.py
+++ Day-3/wires.py
@@ -1,48 +1,45 @@
 wire_a = ['R8','U5','L5','D3']
 wire_b = ['U7','R6','D4','L4']
 
-def grid_points(w):
-	coord = []
-	currPoint= [0, 0]
-	x = currPoint[0]
-	y = currPoint[1]
-	print(x, y)
+def grid_points(w, w_id):
+	#currPoint= (x, y)
+	coords = {}
+	x = 0
+	y = 0
 	for d in w:
 		dirc = d[0]
-		steps = int(d[1])
-		if dirc == 'U':
-			c = 1
-			while c < steps:
-				currPoint = [x, y += 1]
-				coord.append(currPoint)
-				c += 1
-		elif dirc == 'D':
-			c = steps
-			while c > 0:
-				currPoint = [x, y -= 1]
-				coord.append(currPoint)
-				c -= 1
-		elif dirc == 'R':
-			c = 1
-			while c < steps:
-				currPoint = [x += 1, y]
-				coord.append(currPoint)
-				c += 1
-		elif dirc == 'L':
-			c = steps
-			while c > 0:
-				currPoint = [x -= 1, y]
-				coord.append(currPoint)
-				c -= 1
-	print(coord)
+		steps = int(d[1:])
+		for _ in range(0, steps):
+			if dirc == 'U':
+				x += 1
+				currPoint = (x, y)
+				coords[currPoint] = w_id
+			elif dirc == 'D':
+				x -= 1
+				currPoint = (x, y)
+				coords[currPoint] = w_id
+			elif dirc == 'R':
+				y += 1
+				currPoint = (x, y)
+				coords[currPoint] = w_id
+			elif dirc == 'L':
+				y -= 1
+				currPoint = (x , y)
+				coords[currPoint] = w_id
+	print(coords)
+	return coords
 
 def cross_points(a, b):
-	aCoord = []
-	bCoord = []
-
-#find all coordinates for each wire
-# compare to find any common points
+	seen = {}
+	for coord in coords:
+		c = (coords.x, coords.y)
+		prev_wire = seen.get(c)
+		if prev_wire:
+			pass
+	
+#find all coordinates for first wire
+# find next wire, while comparing to first to find any common points
 # find closest cross from start point, not 0,0
 # calculate manhatten distance from sp, x + y 
 
-grid_points(wire_a)		
+grid_points(wire_a, 'a')		
