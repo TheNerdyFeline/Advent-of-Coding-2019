c02ff2af6eecc33897fff097ab68eefe85d0f4eb
theNerdyFeline <jmw5050@gmail.com>
2019-12-18 15:00:43 -0800

    D3P2 clean up

--- Day-3/c-wires.py
+++ Day-3/c-wires.py
@@ -9,24 +9,19 @@ moves = {'U': (+0, +1), 'D': (+0, -1), 'R': (+1, +0), 'L': (-1, +0)}
 for wire, line in enumerate(sys.stdin):
 	x, y = 0, 0
 	ts = 0
-	#print(wire)
 	for move in line.split(','):
 		dx, dy = moves[move[0]]
 		for _ in range(int(move[1:])):
 			x, y = x + dx, y + dy
 			ts += 1
-			#mdist = abs(x) + abs(y)
 			cp = grid.get((x, y), ts)
 			cts = cp + ts
 			if grid.get((x, y)) and wire > 0:
 				cps[(x, y)] = cts
-			if cts < best:
+				if cts < best:
 					best = cts
-					#print('ts', ts, 'cp', cp, 'best', best)
-					print(cps)
 			grid[(x, y)] = ts
 
-print(cps) 
 print(best)
 #293494 too high
 #7961 too low
