96c68f19b526ed855df34bf4795c0f29473a6996
theNerdyFeline <jmw5050@gmail.com>
2019-12-18 15:55:55 -0800

    D3P2 Complete

--- Day-3/c-wires.py
+++ Day-3/c-wires.py
@@ -5,24 +5,29 @@ grid = {}
 cps = {}
 best = math.inf
 moves = {'U': (+0, +1), 'D': (+0, -1), 'R': (+1, +0), 'L': (-1, +0)}
+wire_a = sys.stdin.readline().split(',')
+wire_b = sys.stdin.readline().split(',')
 
-for wire, line in enumerate(sys.stdin):
-	x, y = 0, 0
+def calc_points_steps(path):
+	points = {}
 	ts = 0
-	for move in line.split(','):
+	x, y = 0, 0
+	for move in (path):
 		dx, dy = moves[move[0]]
 		for _ in range(int(move[1:])):
 			x, y = x + dx, y + dy
 			ts += 1
-			cp = grid.get((x, y), ts)
-			cts = cp + ts
-			if grid.get((x, y)) and wire > 0:
-				cps[(x, y)] = cts
-				if cts < best:
-					best = cts
-			grid[(x, y)] = ts
+			if (x, y) not in points:
+				points[(x, y)] = ts
+	return points
 
-print(best)
+a_points = calc_points_steps(wire_a)
+b_points = calc_points_steps(wire_b)
+inter_points = [point for point in a_points if point in b_points]
+
+p2 = min(a_points[point] + b_points[point] for point in inter_points)
+print(p2)
+
 #293494 too high
 #7961 too low
 #6442 too low
