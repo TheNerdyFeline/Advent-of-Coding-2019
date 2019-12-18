c063af9a794dfb61ae5d7a4424905e4982a6067d
theNerdyFeline <jmw5050@gmail.com>
2019-12-18 11:35:47 -0800

    Day 3 Part 2 wip

--- Day-3/c-wires.py
+++ Day-3/c-wires.py
@@ -8,15 +8,21 @@ moves = {'U': (+0, +1), 'D': (+0, -1), 'R': (+1, +0), 'L': (-1, +0)}
 for wire, line in enumerate(sys.stdin):
 	x, y = 0, 0
 	ts = 0
+	#print(wire)
 	for move in line.split(','):
 		dx, dy = moves[move[0]]
 		for _ in range(int(move[1:])):
 			x, y = x + dx, y + dy
 			ts += 1
 			#mdist = abs(x) + abs(y)
-			if grid.get((x, y), wire) != wire:
-				print(x, y, ts)
-				best = mdist
-			grid[(x, y, ts)] = wire
+			cp = grid.get((x, y), ts)
+			cts = cp + ts
+			if grid.get((x, y)) and wire > 0:
+				best = cts
+				print(ts, grid.get(x, y), cp, best)
+			grid[(x, y)] = ts + wire
 
-print(best)
+print(best) 
+#293494 too high
+#7961 too low
+#6442 too low
