3eafbfff5980a019e8441b36a15ba75627cf2ee3
theNerdyFeline <jmw5050@gmail.com>
2019-12-21 13:09:41 -0800

    D4P2 too high

--- Day-4/passwd-check.py
+++ Day-4/passwd-check.py
@@ -1,7 +1,9 @@
 # range 172930-683082
 from operator import eq
+from itertools import groupby
 
 pws = list(range(172930, 683083))
+pp = []
 
 def pass_check(num_range):
 	c = 0
@@ -10,10 +12,30 @@ def pass_check(num_range):
 		if len(i) < 6:
 			pass
 		else:
-			n = [int(d) for d in str(i)]
+			n = [int(d) for d in i]
 			if any(map(eq, i, i[1:])) and sorted(n) == n:
+				pp.append(n)
 				c += 1
+	print('pc1', c)
+	pass_check2(pp)
+
+def pass_check2(a):
+	c = 0
+	#print(pp)
+	for j in a: 
+		runs = [len(list(g)) for _, g in groupby(j)]
+		for r in runs:
+			if r == 2:
+				rs = sum(u for u in runs if u > 1)
+				if rs % 2 == 0:
+					print(j)
+					print(runs)
+					print(rs)
+					c += 1
 	print(c)
 
 #pass_check(list(range(244567, 244667)))
 pass_check(pws)
+# 1675
+# 1341 too high
+#1007 too low
