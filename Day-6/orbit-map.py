ba0c46956e4aa4141bead870b57cee2cc8f5ca4f
theNerdyFeline <jmw5050@gmail.com>
2019-12-29 15:16:57 -0800

    D6P2 Complete

--- Day-6/orbit-map.py
+++ Day-6/orbit-map.py
@@ -9,6 +9,9 @@ def create_map():
 		orb = d[1].rstrip()
 		orbits[orb] = d[0]
 	count_orbits(orbits)
+	my_path = find_path(orbits, 'YOU')
+	santas_path = find_path(orbits, 'SAN')
+	hops_santa(my_path, santas_path)
 
 def count_orbits(inp):
 	oc = 0
@@ -17,7 +20,27 @@ def count_orbits(inp):
 		next_orbit = inp[o]
 		while next_orbit != 'COM':
 			oc += 1
-			next_orbit = inp[next_orbit]
-	print(oc)
+			next_orbit = inp[next_orbit] 
+	print('p1', oc)
+
+def find_path(inp, start):
+	oc = 0
+	path = []
+	loc = inp[start]
+	while loc != 'COM':
+		path.append(loc)
+		loc = inp[loc]
+	return path
+			
+def hops_santa(my_path, santa_path):
+	oc = 0
+	for obj in my_path:
+		if obj in santa_path:
+			hops_santa_parent = santa_path.index(obj)
+			hops_my_parent = my_path.index(obj)
+			oc = hops_santa_parent + hops_my_parent
+			break
+	print('p2', oc)
+
 
 create_map()
